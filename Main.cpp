99b0172c4db39d53024146322e86b380858cd6e2
Matt G <Im.Bee.Vibe@gmail.com>
2023-12-17 01:17:27 +0100

    GGs at the end

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,10 @@
-#include <chrono>
-#include <thread>
-
 #include "Board.hpp"
 #include "GameLogic.hpp"
 #include "PlayerInput.h"
+
+#include <chrono>
+#include <thread>
+#include <iostream>
 
 #define BOARD_SIZE_W 80
 #define BOARD_SIZE_H 16
@@ -29,4 +30,13 @@ int main()
         std::this_thread::sleep_for(GAME_UPDATE_LATENCY);
     }
 
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif // _WIN32
+
+    std::cout << "The end" << std::endl;
+    std::cout << "GGs";
+    std::this_thread::sleep_for(GAME_UPDATE_LATENCY * 25);
 }
